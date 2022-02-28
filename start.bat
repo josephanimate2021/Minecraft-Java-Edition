2a12b50a3ddd1ce7db669e501f4d50506f529804
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-02-28 06:38:52 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,11 +2,11 @@
 for /f "delims=" %%i in ('java -version 2^>nul') do set output=%%i
 IF "!output!" EQU "" (
 	echo Java could not be found. Exiting...
-  PING -n 3 127.0.0.1>nul
+  PING -n 6 127.0.0.1>nul
 ) else (
 	echo Java is installed. Starting Up Minecraft Java Edition...
-  PING -n 3 127.0.0.1>nul
+  PING -n 6 127.0.0.1>nul
 	java -jar minecraft.jar
   echo Minecraft Java Edition Has Been Started. Exiting...
-  PING -n 3 127.0.0.1>nul
+  PING -n 6 127.0.0.1>nul
 )
