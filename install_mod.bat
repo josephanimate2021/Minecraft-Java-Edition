fe1b21e29040cb8fbfb67ac26eb1244b6a9cfd54
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-03-06 21:12:31 -0500

    Create install_mod.bat

---
+++ install_mod.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Starting Up The Minecraft Mod Installer...
+PING -n 6 127.0.0.1>nul
+call java\bin\java.exe -jar install_mod.jar
+echo The Minecraft Mod Thing has now been installed. Exiting...
+PING -n 6 127.0.0.1>nul
