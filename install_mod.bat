5777bf87d98cb4f46b9e67653ad89b87aeada77c
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-03-06 21:30:10 -0500

    Update install_mod.bat

--- install_mod.bat
+++ install_mod.bat
@@ -1,10 +1,6 @@
 @echo off
-if !MINECRAFTMODISINSTALLED!==y (
-exit
-)
 echo Starting Up The Minecraft Mod Installer...
 PING -n 6 127.0.0.1>nul
 call java\bin\java.exe -jar install_mod.jar
 echo The Minecraft Mod Thing has now been installed. Exiting...
-set MINECRAFTMODISINSTALLED==y
 PING -n 6 127.0.0.1>nul
